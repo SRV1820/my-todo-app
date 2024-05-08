bb474afbca5f4e3be0bef9b02b401698448d429f
srodriguez <sharon1820rodriguez@gmail.com>
2024-05-08 17:34:53 -0600

    Initial commit

--- functions.py
+++ functions.py
@@ -1,9 +1,7 @@
 FILEPATH = "C:\\Users\Guess\Documents\Python Training\\venv\\todos.txt"
 
-
 def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of to-do items"""
-
 
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
